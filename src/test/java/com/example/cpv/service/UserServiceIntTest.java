094233459fc25ad1b73030bc018f0caf4519b60f
sivakoushik <sivakoushik.d@autorabit.com>
2021-06-25 14:01:07 +0530

    New Status

--- src/test/java/com/example/cpv/service/UserServiceIntTest.java
+++ src/test/java/com/example/cpv/service/UserServiceIntTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.*;
 
 /**
  * Test class for the UserResource REST controller.
- * 
+ *   
  * @see UserService
  */
 @RunWith(SpringJUnit4ClassRunner.class)
