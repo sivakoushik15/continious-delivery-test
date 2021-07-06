5c0a05d0f2c6f3abda8bb4cbda527c145de9a5f7
sivakoushik <sivakoushik.d@autorabit.com>
2021-07-06 14:03:21 +0530

    git status add

--- src/test/java/com/example/cpv/service/UserServiceIntTest.java
+++ src/test/java/com/example/cpv/service/UserServiceIntTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.*;
 
 /**
  * Test class for the UserResource REST controller.
- *   
+ *   101
  * @see UserService
  */
 @RunWith(SpringJUnit4ClassRunner.class)
