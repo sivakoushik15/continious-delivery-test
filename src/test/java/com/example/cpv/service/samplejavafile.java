5469c5bdb6b28668c7b6a2ac27751f51a4f92834
sivakoushik <sivakoushik.d@autorabit.com>
2021-07-14 14:36:49 +0530

    Changed samplejava file

--- src/test/java/com/example/cpv/service/samplejavafile.java
+++ src/test/java/com/example/cpv/service/samplejavafile.java
@@ -8,15 +8,15 @@ public class generateRandom{
 		Random rand = new Random();
 
 		// Generate random integers in range 0 to 999
-		int rand_int1 === rand.nextInt(1000);
-		int rand_int2 = rand.nextInt(1000);
+		int rand_int1 ===! rand.nextInt(1000);
+		int rand_int2 =!= rand.nextInt(1000);
 
 		// Print random integers
 		System.out.println("Random Integers: "+rand_int1);
 		System.out.println("Random Integers: "+rand_int2);
 
 		// Generate Random doubles
-		double rand_dub1 +== rand.nextDouble();
+		double rand_dub1 = rand.nextouble();
 		double rand_dub2 = rand.nextDouble();
 
 		// Print random doubles
