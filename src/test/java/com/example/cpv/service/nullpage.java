49de080f788de2a962fb801481e6feeda9a7d93e
Siva Koushik <52826588+sivakoushik15@users.noreply.github.com>
2021-09-02 10:55:06 +0530

    Revert "Update nullpage.java"

--- src/test/java/com/example/cpv/service/nullpage.java
+++ src/test/java/com/example/cpv/service/nullpage.java
@@ -1,25 +1,42 @@
+// To use randomUUID function.
+import java.util.UUID;
 import java.io.*;
-  
-class GFG
+
+class Singleton
 {
-    public static void main (String[] args)
-    {
-        // Initializing String variable with null value
-        String ptr = null;
-  
-        // Checking if ptr.equals null or works fine.
-        try
-        {
-            // This line of code throws NullPointerException
-            // because ptr is null
-            if (ptr.equals("gfg"))
-                System.out.print("Same");
-            else 
-                System.out.print("Not Same");
-        }
-        catch(NullPointerException e)
-        {
-            System.out.print("NullPointerException Caught");
-        }
-    }
+	// Initializing values of single and ID to null.
+	private static Singleton single = null;
+	private String ID = null;
+
+	private Singleton()
+	{
+		/* Make it private, in order to prevent the
+		creation of new instances of the Singleton
+		class. */
+
+		// Create a random ID
+		ID = UUID.randomUUID().toString();
+	}
+
+	public static Singleton getInstance()
+	{
+		if (single == null)
+			single = new Singleton();
+		return single;
+	}
+
+	public String getID()
+	{
+		return this.ID;
+	}
+}
+
+// Driver Code
+public class TestSingleton
+{
+	public static void main(String[] args)
+	{
+		Singleton s = Singleton.getInstance();
+		System.out.println(s.getID());
+	}
 }
